154d3dde6238878d674dfca2837fcda69cf48291
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-02-20 22:07:19 -0800

    Setup Sphinx conf to import module for autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,23 @@ copyright = '2025, Elijah Greenstein'
 author = 'Elijah Greenstein'
 release = '0.1'
 
+# -- Importing modules -------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
+#
+# autodoc analyses the code and docstrings by introspection after importing the
+# modules. For importing to work, you have to make sure that your modules can be
+# found by Sphinx and that dependencies can be resolved (if your module does
+# import foo, but foo is not available in the python environment that Sphinx
+# runs in, your module import will fail).
+#
+# It is alternatively possible to patch the Sphinx run so that it can operate
+# directly on the sources; e.g. if you want to be able to do a Sphinx build from
+# a source checkout.
+
+import pathlib
+import sys
+sys.path.insert(0, str(pathlib.Path('..', '..', 'src').resolve()))
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 

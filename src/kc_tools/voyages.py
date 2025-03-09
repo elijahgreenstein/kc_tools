db85391347cc07b17f87b12368bf10ed2e5fccc5
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-03-08 21:32:09 -0800

    Restructure input data

--- src/kc_tools/voyages.py
+++ src/kc_tools/voyages.py
@@ -4,23 +4,24 @@
 import geopandas as gpd
 import pandas as pd
 
-def get_sequence(traj_df, port_df, t1="t1", t2="t2", line="line", port="port",
-                 geometry="geometry"):
+def get_sequence(traj_df, port_names, port_geom, t1="t1", t2="t2", line="line"):
     """Generate a dataframe column containing ports of call.
 
     :param traj_df: Dataframe of line segments.
     :type traj_df: pd.DataFrame
-    :param port_df: Dataframe of port names and geometries.
-    :type port_df: geopandas.GeoDataFrame
+    :param port_names: List of port names.
+    :type port_df: list, pd.Series
+    :param port_geom: List of port geometries.
+    :type port_df: list, pd.Series
     :param t1: Name of column in ``traj_df`` containing start times for each line segment.
     :type t1: str, default: "t1"
     :param t2: Name of column in ``traj_df`` containing end times for each line segment.
     :type t2: str, default: "t2"
     :param line: Name of column in ``traj_df`` containing line geometries.
     :type line: str, default: "line"
-    :param port: Name of column in ``port_df`` containing names of ports.
-    :type port: str, default: "port"
-    :param geometry: Name of column in ``port_df`` containing port geometries.
-    :type geometry: str, default: "geometry"
     """
+
+    # TODO: Check intersection with each port.
+    # - Filter for at least one intersection:
+    # df[df[port_names].sum(axis=1) > 0]
     pass

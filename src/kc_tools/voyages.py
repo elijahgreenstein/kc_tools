27beeb6a400b3ab01c9f65d1e758bea1549e2f1c
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-03-11 12:15:48 -0700

    Update plan

--- src/kc_tools/voyages.py
+++ src/kc_tools/voyages.py
@@ -4,15 +4,13 @@
 import geopandas as gpd
 import pandas as pd
 
-def get_sequence(traj_df, port_names, port_geom, t1="t1", t2="t2", line="line"):
+def get_sequence(traj_df, ports, t1="t1", t2="t2", line="line"):
     """Generate a dataframe column containing ports of call.
 
     :param traj_df: Dataframe of line segments.
     :type traj_df: pd.DataFrame
-    :param port_names: List of port names.
-    :type port_df: list, pd.Series
-    :param port_geom: List of port geometries.
-    :type port_df: list, pd.Series
+    :param ports: Dictionary of port names and geometries.
+    :type ports: dict
     :param t1: Name of column in ``traj_df`` containing start times for each line segment.
     :type t1: str, default: "t1"
     :param t2: Name of column in ``traj_df`` containing end times for each line segment.
@@ -24,4 +22,9 @@ def get_sequence(traj_df, port_names, port_geom, t1="t1", t2="t2", line="line"):
     # TODO: Check intersection with each port.
     # - Filter for at least one intersection:
     # df[df[port_names].sum(axis=1) > 0]
+
+    # Will have several columns of port_names
+    # Get indicator variables for intersections, change to integer 0, 1
+    # df[port_names] = df[port_names].astype(int)
+    # Create a column indicating overlaps (sum plus offset)
     pass

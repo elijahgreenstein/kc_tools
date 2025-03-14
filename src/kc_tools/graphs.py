ff329908f2f81a26637e516bce84b05398e50b10
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-03-14 12:07:39 -0700

    Update style; remove digraph function

--- src/kc_tools/graphs.py
+++ src/kc_tools/graphs.py
@@ -152,15 +152,8 @@ def get_node_seq(
     return (node_seq, unk)
 
 
-def add_edges_GDL(
-    node_seq,
-    GDL,
-    breaks="_BREAK",
-    weighted=True,
-    self_loops=False
-):
-    """Add edges to a "graph of direct linkages" (GDL).
-    """
+def add_edges_GDL(node_seq, GDL, breaks="_BREAK", weighted=True, self_loops=False):
+    """Add edges to a "graph of direct linkages" (GDL)."""
     # Ensure that graph is nx.DiGraph
     if type(GDL) != nx.DiGraph:
         raise TypeError("GDL must be a networkx DiGraph.")
@@ -178,15 +171,8 @@ def add_edges_GDL(
         return graph
 
 
-def add_edges_GAL(
-    node_seq,
-    GAL,
-    breaks="_BREAK",
-    weighted=True,
-    self_loops=False
-):
-    """Add edges to a "graph of all linkages" (GAL).
-    """
+def add_edges_GAL(node_seq, GAL, breaks="_BREAK", weighted=True, self_loops=False):
+    """Add edges to a "graph of all linkages" (GAL)."""
     # Ensure that graph is nx.Graph
     if type(GAL) != nx.Graph:
         raise TypeError("GAL must be a networkx Graph.")
@@ -214,61 +200,3 @@ def add_edges_GAL(
         else:
             graph.add_edges_from(edges)
         return graph
-
-
-def make_digraph(
-    edges,
-    from_node="node1",
-    to_node="node2",
-    weighted=True,
-    remove_unknown=True,
-    unk="_UNKNOWN",
-    breaks="_BREAK",
-    self_loops=False,
-):
-    """Generate a digraph from a dataframe of edges.
-
-    :param edges: Dataframe of edges.
-    :type edges: pd.DataFrame
-    :param from_node: Name of the column with origin node.
-    :type from_node: str, "node1"
-    :param to_node: Name of the column with the target node.
-    :type to_node: str, default: "node2"
-    :param weighted: Optionally add weight of ``1`` for each edge.
-    :type weighted: bool, default: True
-    :param remove_unknown: Remove edges with unknown nodes.
-    :type remove_unknown: bool, default: True
-    :param unk: Node string indicating a "stop" without an intersection.
-    :type unk: str, default: "_UNKNOWN"
-    :param breaks: String indicating a "break" in the graph.
-    :type breaks: str, default: "_BREAK"
-    :param self_loops: If true, allow self-loop edges.
-    :type self_loops: bool, default: False
-    """
-    # Initialize directed graph
-    DG = nx.DiGraph()
-
-    # Optionally remove "stops" without a node intersection
-    if remove_unknown:
-        data = edges[(edges[from_node] != unk) & (edges[to_node] != unk)].copy()
-    else:
-        data = edges.copy()
-
-    # Get the sequence of target nodes
-    seq = data[to_node].values
-
-    for idx_to in range(1, len(seq)):  # Iterate over targets from index 1
-        idx_from = idx_to - 1
-        n1 = seq[idx_from]
-        n2 = seq[idx_to]
-        if (n1 == breaks) or (n2 == breaks):
-            pass  # Skip the edge if it is on a "break"
-        elif (n1 != n2) or (n1 == n2 and self_loops):
-            # Increment weight if generating a weighted graph
-            if DG.has_edge(n1, n2) and weighted:
-                DG[n1][n2]["weight"] += 1
-            else:
-                DG.add_edge(n1, n2, weight=1)
-        else:
-            pass
-    return DG
